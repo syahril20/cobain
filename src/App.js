58a24d0fccdf78631f82c3055fe61ef72d2e2eb3
syahril20 <rodyulo20@gmail.com>
2023-02-23 11:59:43 +0700

    Sokin

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
+import React from 'react';
 import './App.css';
-// import Body from './Page/Body';
 import Login from './Page/Login';
 import'./Page/Login.css';
 
 const App = () => {
   return(
     <div>
+      <header className='App-header'>
       <div></div>
       <Login/>
+      </header>
     </div>
   );
 }
--- src/Page/Login.js
+++ src/Page/Login.js
@@ -4,22 +4,23 @@ import './Login.css';
 const Login = () => {
     return (
         <div>
-            <head>
-                <link rel="preconnect" href="https://fonts.googleapis.com" />
-                <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
-                <link href="https://fonts.googleapis.com/css2?family=Comic+Neue&family=Shantell+Sans:ital@1&display=swap" rel="stylesheet" />
-            </head>
-            <header className='header'>
-                <div className='box'>
-                    <div className='text'>
-                        <p>Login</p>
+            <link rel="preconnect" href="https://fonts.googleapis.com" />
+            <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
+            <link href="https://fonts.googleapis.com/css2?family=Comic+Neue&family=Shantell+Sans:ital@1&display=swap" rel="stylesheet" />
+            <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.3.0/css/all.min.css" integrity="sha512-SzlrxWUlpfuzQ+pcUCosxcglQRNAq/DZjVsC0lE40xsADsfeQoEypE+enwcOiGjk/bSuGGKHEyjSoQ1zVisanQ==" crossOrigin="anonymous" referrerPolicy="no-referrer" />
+            <div className='box'>
+                <div className='text'>
+                    <p>Login</p>
+                    <div>
+                        <i className="fa-solid fa-user icon"></i>
+                        <input type={'text'} className='textBox' id='Username'></input>
                     </div>
+                    <input type={'password'} className='textBox' id='Password'></input><br />
+                    <button type='submit' className='button' id='submit'>Submit</button><br />
+                    <button type='button' className='button' id='Register'>Register</button><br />
                 </div>
-            </header>
-
+            </div>
         </div>
-
     );
 }
-
 export default Login;
